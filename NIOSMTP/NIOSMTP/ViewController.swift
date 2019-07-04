d304427b6a771bcd2e991a950a002acaee405e76
George Barnett <gbarnett@apple.com>
2019-07-04 12:38:53 +0100

    Use NIO when NIOTS isn't available (#22)
    * Use NIO when NIOTS isn't available
    
    Motivation:
    
    It's useful to have an example of how people can fall back to NIO when
    NIOTS isn't available.
    
    Modifications:
    
    Add support to fallback to using NIO when NIOTS isn't available.
    
    Result:
    
    NIOSMTP can now run on iOS 10 and iOS 11 where NIOTS isn't available.

--- NIOSMTP/NIOSMTP/ViewController.swift
+++ NIOSMTP/NIOSMTP/ViewController.swift
@@ -16,31 +16,36 @@ import UIKit
 import NIO
 import NIOExtras
 import NIOTransportServices
+import NIOTLS
+import NIOSSL
 
+#if canImport(Network)
 import Network
+#endif
 
 func sendEmail(_ email: Email,
-               group: NIOTSEventLoopGroup,
+               group: EventLoopGroup,
                communicationHandler: @escaping (String) -> Void,
                _ handler: @escaping (Error?) -> Void) {
     let emailSentPromise: EventLoopPromise<Void> = group.next().makePromise()
-    let configuration = NIOTSConnectionBootstrap.config
 
-    let connection = NIOTSConnectionBootstrap(group: group)
-        .channelInitializer { channel in
-            channel.pipeline.addHandlers([
-                PrintEverythingHandler(handler: communicationHandler),
-                ByteToMessageHandler(LineBasedFrameDecoder()),
-                SMTPResponseDecoder(),
-                MessageToByteHandler(SMTPRequestEncoder()),
-                SendEmailHandler(configuration: configuration,
-                                 email: email,
-                                 allDonePromise: emailSentPromise)
-                ], position: .last)
-        }
-        .tlsConfig()
-        .connect(host: configuration.hostname,
-                 port: configuration.port)
+    let bootstrap: ClientBootstrapProtocol
+
+    if #available(OSX 10.14, iOS 12.0, tvOS 12.0, watchOS 6.0, *), let tsGroup = group as? NIOTSEventLoopGroup {
+        bootstrap = configureNIOTSBootstrap(group: tsGroup,
+                                            email: email,
+                                            emailSentPromise: emailSentPromise,
+                                            communicationHandler: communicationHandler)
+    } else {
+        bootstrap = configureBootstrap(group: group,
+                                       email: email,
+                                       emailSentPromise: emailSentPromise,
+                                       communicationHandler: communicationHandler)
+    }
+
+    let connection = bootstrap.connect(host: Configuration.shared.serverConfig.hostname,
+                                       port: Configuration.shared.serverConfig.port)
+
     connection.cascadeFailure(to: emailSentPromise)
     emailSentPromise.futureResult.map {
         connection.whenSuccess { $0.close(promise: nil) }
@@ -53,8 +58,7 @@ func sendEmail(_ email: Email,
 
 
 class ViewController: UIViewController {
-
-    var group: NIOTSEventLoopGroup? = nil
+    var group: EventLoopGroup? = nil
 
     @IBOutlet weak var sendEmailButton: UIButton!
     @IBOutlet weak var logView: UITextView!
@@ -89,7 +93,7 @@ class ViewController: UIViewController {
     }
 
     override func viewWillAppear(_ animated: Bool) {
-        let group = NIOTSEventLoopGroup(loopCount: 1, defaultQoS: .utility)
+        let group = makeEventLoopGroup(loopCount: 1, implementation: .best)
         self.group = group
     }
 
@@ -104,3 +108,113 @@ class ViewController: UIViewController {
     }
 }
 
+// MARK: - NIO/NIOTS handling
+
+/// This protocol is intended as a layer of abstraction over `ClientBootstrap` and
+/// `NIOTSConnectionBootstrap`. We only need a `connect` method since configuration
+/// is done on the concrete type.
+protocol ClientBootstrapProtocol {
+    func connect(host: String, port: Int) -> EventLoopFuture<Channel>
+}
+
+extension ClientBootstrap: ClientBootstrapProtocol {}
+
+#if canImport(Network)
+@available(OSX 10.14, iOS 12.0, tvOS 12.0, watchOS 6.0, *)
+extension NIOTSConnectionBootstrap: ClientBootstrapProtocol {}
+#endif
+
+@available(OSX 10.14, iOS 12.0, tvOS 12.0, watchOS 6.0, *)
+func configureNIOTSBootstrap(group: NIOTSEventLoopGroup,
+                             email: Email,
+                             emailSentPromise: EventLoopPromise<Void>,
+                             communicationHandler: @escaping (String) -> Void) -> ClientBootstrapProtocol {
+    let bootstrap = NIOTSConnectionBootstrap(group: group).channelInitializer { channel in
+        channel.pipeline.addHandlers([
+            PrintEverythingHandler(handler: communicationHandler),
+            ByteToMessageHandler(LineBasedFrameDecoder()),
+            SMTPResponseDecoder(),
+            MessageToByteHandler(SMTPRequestEncoder()),
+            SendEmailHandler(configuration: Configuration.shared.serverConfig,
+                             email: email,
+                             allDonePromise: emailSentPromise)
+            ], position: .last)
+    }
+
+    if Configuration.shared.useTLS {
+        return bootstrap.tlsOptions(.init())
+    } else {
+        return bootstrap
+    }
+}
+
+func configureBootstrap(group: EventLoopGroup,
+                        email: Email,
+                        emailSentPromise: EventLoopPromise<Void>,
+                        communicationHandler: @escaping (String) -> Void) -> ClientBootstrapProtocol {
+    return ClientBootstrap(group: group).channelInitializer { channel in
+        var handlers: [ChannelHandler] = [
+            PrintEverythingHandler(handler: communicationHandler),
+            ByteToMessageHandler(LineBasedFrameDecoder()),
+            SMTPResponseDecoder(),
+            MessageToByteHandler(SMTPRequestEncoder()),
+            SendEmailHandler(configuration: Configuration.shared.serverConfig,
+                             email: email,
+                             allDonePromise: emailSentPromise)
+        ]
+
+        if Configuration.shared.useTLS {
+            do {
+                let sslContext = try NIOSSLContext(configuration: .forClient())
+                let sslHandler = try NIOSSLClientHandler(context: sslContext,
+                                                         serverHostname: Configuration.shared.serverConfig.hostname)
+                handlers.insert(sslHandler, at: 0)
+            } catch {
+                return channel.eventLoop.makeFailedFuture(error)
+            }
+        }
+
+        return channel.pipeline.addHandlers(handlers, position: .last)
+    }
+}
+
+/// Network implementation and by extension which version of NIO to use.
+enum NetworkImplementation {
+    /// POSIX sockets and NIO.
+    case posix
+
+    #if canImport(Network)
+    @available(OSX 10.14, iOS 12.0, tvOS 12.0, watchOS 6.0, *)
+    /// NIOTransportServices (and Network.framework).
+    case transportServices
+    #endif
+
+    /// Return the best implementation available for this platform, that is NIOTransportServices
+    /// when it is available or POSIX and NIO otherwise.
+    static var best: NetworkImplementation {
+        if #available(OSX 10.14, iOS 12.0, tvOS 12.0, watchOS 6.0, *) {
+            return .transportServices
+        } else {
+            return .posix
+        }
+    }
+}
+
+/// Makes an appropriate `EventLoopGroup` based on the given implementation.
+///
+/// For `.posix` this is a `MultiThreadedEventLoopGroup`, for `.networkFramework` it is a
+/// `NIOTSEventLoopGroup`.
+///
+/// - Parameter implementation: The network implementation to use.
+func makeEventLoopGroup(loopCount: Int, implementation: NetworkImplementation) -> EventLoopGroup {
+    switch implementation {
+    case .transportServices:
+        guard #available(OSX 10.14, iOS 12.0, tvOS 12.0, watchOS 6.0, *) else {
+            // This is gated by the availability of `.networkFramework` so should never happen.
+            fatalError(".networkFramework is being used on an unsupported platform")
+        }
+        return NIOTSEventLoopGroup(loopCount: loopCount, defaultQoS: .utility)
+    case .posix:
+        return MultiThreadedEventLoopGroup(numberOfThreads: loopCount)
+    }
+}
