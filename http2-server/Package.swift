b6895f3fe9a07d19bb49b22353ca269f582082ad
Johannes Weiss <johannesweiss@apple.com>
2019-08-01 11:54:48 +0100

    example HTTP/2 server
    Motivation:
    
    We need better and more real-world examples because configuring HTTP/2
    isn't that easy because it always requires TLS and ALPN to be set up
    appropriately.
    
    Motifications:
    
    Set up the most simple HTTP/2 server over TLS.
    
    Result:
    
    More examples.

---
+++ http2-server/Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version:5.1
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2019 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import PackageDescription
+
+let package = Package(
+    name: "http2-server",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.5.1"),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.2.0"),
+        .package(url: "https://github.com/apple/swift-nio-http2.git", from: "1.4.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "http2-server",
+            dependencies: ["NIO", "NIOHTTP1", "NIOHTTP2", "NIOSSL"]),
+    ]
+)
---
+++ http2-server/README.md
@@ -0,0 +1,9 @@
+# http2-server
+
+This is a very simple example HTTP/2 server that configures TLS (and ALPN)
+similar to how you would configure it in a real system.
+
+To run, just open the project in Xcode or type
+
+    swift run
+
