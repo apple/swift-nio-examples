2e1d9e6136d573612f308576d2e2e71ff5286756
David Evans <d.evans@apple.com>
2020-05-15 14:41:21 +0100

    Remove Linux test files

--- connection-limiter/Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import connection_limiterTests
-
-var tests = [XCTestCaseEntry]()
-tests += connection_limiterTests.allTests()
-XCTMain(tests)
--- connection-limiter/Tests/connection-limiterTests/XCTestManifests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-#if !canImport(ObjectiveC)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(connection_limiterTests.allTests),
-    ]
-}
-#endif
