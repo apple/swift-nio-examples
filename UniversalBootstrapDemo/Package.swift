4b72f36b6ba42533630c4436be43883894d78f94
nomo-nomad <nomo-nomad@bisonware.tech>
2021-01-11 16:08:33 -0500

    Update minimum version of NIO to 2.20.0

--- UniversalBootstrapDemo/Package.swift
+++ UniversalBootstrapDemo/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "UniversalBootstrapDemo",
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.16.1"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.20.0"),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.7.1"),
         .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.5.1"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "0.0.5"))
