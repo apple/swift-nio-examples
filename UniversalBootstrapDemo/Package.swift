e9949b9367565b5c190c88cba57b14ec366ebf5f
Oliver Epper <oliver.epper@gmail.com>
2021-10-29 14:10:32 +0200

    Updated dependencies

--- UniversalBootstrapDemo/Package.swift
+++ UniversalBootstrapDemo/Package.swift
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
     name: "UniversalBootstrapDemo",
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.20.0"),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.7.1"),
-        .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.5.1"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "0.0.5"))
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.33.0"),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.16.1"),
+        .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.11.3"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.1")
     ],
     targets: [
         .target(
