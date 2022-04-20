980effae4bf8d750aa1983aa696608322deb9ca7
Fabian Fett <fabianfett@apple.com>
2022-04-20 10:51:45 +0200

    Require Swift 5.4 (#76)
    Since we only support Swift 5.4 and later into the future, we should require it in all examples.

--- http2-server/Package.swift
+++ http2-server/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.4
 //===----------------------------------------------------------------------===//
 //
 // This source file is part of the SwiftNIO open source project
