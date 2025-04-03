3be0c6b0cb11e2960c9cbff20445f0b9d7130a7f
Rick Newton-Rogers <rnro@apple.com>
2025-04-03 09:49:24 +0100

    backpressure-file-io-channel strict concurrency

--- backpressure-file-io-channel/Package.swift
+++ backpressure-file-io-channel/Package.swift
@@ -1,5 +1,23 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.9
 import PackageDescription
+
+let strictConcurrencyDevelopment = false
+
+let strictConcurrencySettings: [SwiftSetting] = {
+    var initialSettings: [SwiftSetting] = []
+    initialSettings.append(contentsOf: [
+        .enableUpcomingFeature("StrictConcurrency"),
+        .enableUpcomingFeature("InferSendableFromCaptures"),
+    ])
+
+    if strictConcurrencyDevelopment {
+        // -warnings-as-errors here is a workaround so that IDE-based development can
+        // get tripped up on -require-explicit-sendable.
+        initialSettings.append(.unsafeFlags(["-require-explicit-sendable", "-warnings-as-errors"]))
+    }
+
+    return initialSettings
+}()
 
 let package = Package(
     name: "backpressure-file-io-channel",
@@ -7,7 +25,7 @@ let package = Package(
         .macOS(.v10_15), .iOS(.v13), .tvOS(.v13)
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.42.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.81.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.1.0"),
     ],
     targets: [
@@ -26,13 +44,17 @@ let package = Package(
                 .product(name: "NIOCore", package: "swift-nio"),
                 .product(name: "NIOPosix", package: "swift-nio"),
                 .product(name: "Logging", package: "swift-log"),
-            ]),
+            ],
+            swiftSettings: strictConcurrencySettings
+        ),
         .testTarget(
             name: "BackpressureChannelToFileIOTests",
             dependencies: [
                 "BackpressureChannelToFileIO",
                 .product(name: "NIOCore", package: "swift-nio"),
                 .product(name: "NIOPosix", package: "swift-nio"),
-            ]),
+            ],
+            swiftSettings: strictConcurrencySettings
+        ),
     ]
 )
