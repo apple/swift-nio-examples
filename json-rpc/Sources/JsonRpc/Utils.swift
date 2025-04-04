e34998ab84d5f1f4713f0cd46b78a508b0054c56
Rick Newton-Rogers <rnro@apple.com>
2025-04-04 10:38:29 +0100

    license check

--- json-rpc/Sources/JsonRpc/Utils.swift
+++ json-rpc/Sources/JsonRpc/Utils.swift
@@ -1,3 +1,18 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2025 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+
 import Foundation
 
 public enum ResultType<Value, Error>: Sendable where Value: Sendable, Error: Sendable {
